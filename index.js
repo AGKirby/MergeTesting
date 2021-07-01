d61c5d00ec906d47c7f53e4814f6fcd2cdba3f7f
tobyloki5 <tobyloki5@gmail.com>
2021-06-30 17:35:49 -0700

    test case for sum

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
+const assert = require('assert');
+
 main();
 
 async function main() {
-	console.log('hello there');
-	console.log("Greetings")
+	// console.log('hello there');
+	// console.log('Greetings');
 
+	try {
+		console.log('testing sum');
+		assert(add(1, 2) == 4);
+	} catch (e) {
+		console.log(e);
+	}
+
+	console.log('done');
 }
 
 function multiply(x, y) {
-	return x * y
+	return x * y;
 }
 
-function divide(x, y){
-	if(y == 0) {
-		console.log("You're weird")
-		return 0
+function divide(x, y) {
+	if (y == 0) {
+		console.log("You're weird");
+		return 0;
 	}
-	return x / y
+	return x / y;
 }
 
-function squareRoot(x){
-	return Math.sqrt(x)
+function squareRoot(x) {
+	return Math.sqrt(x);
 }
 
 function add(a, b) {
