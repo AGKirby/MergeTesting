9b8c32109786faf6ae8b1025c2504c648b3cc273
AGKirby <adamgraneto2@gmail.com>
2021-06-30 17:32:49 -0700

    added functionality for multiply, divide, and squareRoot

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ main();
 
 async function main() {
 	console.log('hello there');
+	console.log("Greetings")
+
+}
+
+function multiply(x, y) {
+	return x * y
+}
+
+function divide(x, y){
+	if(y == 0) {
+		console.log("You're weird")
+		return 0
+	}
+	return x / y
+}
+
+function squareRoot(x){
+	return Math.sqrt(x)
 }
