d71693b3f195a4b8ae1fab0c5f5c2e0ef12b307a
tobyloki5 <tobyloki5@gmail.com>
2021-06-30 17:33:02 -0700

    add, subtract, power

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ main();
 async function main() {
 	console.log('hello there');
 }
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function power(a, b) {
+	return a ** b;
+}
