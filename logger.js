c02f0d194d9945860dab524ded7bc4ae3301bcb5
AGKirby <adamgraneto2@gmail.com>
2021-07-06 10:57:36 -0700

    Removed the Adam functions :(

--- logger.js
+++ logger.js
@@ -2,13 +2,6 @@ exports.Info = (msg) => {
 	console.log(`${new Date()} - INFO: ${msg}`);
 };
 
-exports.greet = () => {
-	return 'Hello there';
-};
-
-exports.adam = () => {
-	console.log('Adam is the coolest');
-};
 exports.Warn = (msg) => {
 	console.log(`${new Date()} - WARN: ${msg}`);
 };
